00d0a9092301bed80cad31f99aa204bd4ef5bad7
biharijobsarthi <mahidigital85@gmail.com>
2026-07-09 08:23:29 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,9 @@
-
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyC0n0qh2tKviBuLgCroVBBIWowLDpAoDBA",
+  authDomain: "bihari-job-sarthi.firebaseapp.com",
+  projectId: "bihari-job-sarthi",
+  storageBucket: "bihari-job-sarthi.firebasestorage.app",
+  messagingSenderId: "113760321124",
+  appId: "1:113760321124:web:b6deffc7b348a013a6a971"
+};
